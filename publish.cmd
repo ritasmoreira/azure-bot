7068e85ec6448ecd8c22093cb998583406e65ca1
ritasmoreira <ritasmoreira@users.noreply.github.com>
2018-02-21 21:02:59 +0000

    Azure Bot

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=bottesttracking-Web-Deploy.pubxml -p:Password=bsGwbtgjLkASizsEfpDRpqwF5T5ej5PkfxqLylhpj4v9s9qnikDgSgz2qr7t
+
