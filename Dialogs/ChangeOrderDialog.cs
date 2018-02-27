18d9e554eb08dd1c0042384e581e0d3ced167d51
Rita Moreira~ <rita.moreira@xpand-it.com>
2018-02-27 09:42:22 +0000

    testing try times

--- Dialogs/ChangeOrderDialog.cs
+++ Dialogs/ChangeOrderDialog.cs
@@ -48,7 +48,7 @@ namespace LuisBot.Dialogs
             if (!context.UserData.TryGetValue(ContextConstants.Date, out orderDate))
             {
                 await context.PostAsync($"Estou a definir a data pela primeira vez");
-                PromptDialog.Text(context, this.ResumeAfterDouble, "What number?", "Tenta outra vez");
+                PromptDialog.Text(context, this.ResumeAfterDouble, "What number?", "Tenta outra vez", 3);
                 // PromptDialog.Text(context, this.ResumeAfterPrompt, "Qual a data da sua encomenda?", "Data inválida. Por favor tente outra vez", 3);
                 
             }
@@ -75,6 +75,7 @@ namespace LuisBot.Dialogs
         {
             var nr = await result;
             await context.PostAsync($"Number is {nr}");
+            
         }
 
 
